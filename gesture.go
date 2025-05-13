0e980cc94b8c6a7a972d9065d6ccf2a1945a2dd7
HMasataka <masataka.hisasue@gmail.com>
2025-05-13 18:28:03 +0900

    Feat add some gesture

--- gesture.go
+++ gesture.go
@@ -8,20 +8,112 @@ var AllGestures *orderedmap.OrderedMap[string, Gesture]
 
 func init() {
 	AllGestures = orderedmap.New[string, Gesture](orderedmap.WithInitialData(
+		// 四方向
 		orderedmap.Pair[string, Gesture]{
-			Key:   "Up",
+			Key:   "上左下右",
+			Value: []Direction{Up, Left, Down, Right},
+		},
+		orderedmap.Pair[string, Gesture]{
+			Key:   "上右下左",
+			Value: []Direction{Up, Right, Down, Left},
+		},
+		orderedmap.Pair[string, Gesture]{
+			Key:   "右上左下",
+			Value: []Direction{Right, Up, Left, Down},
+		},
+		orderedmap.Pair[string, Gesture]{
+			Key:   "右下左上",
+			Value: []Direction{Right, Down, Left, Up},
+		},
+		orderedmap.Pair[string, Gesture]{
+			Key:   "下左上右",
+			Value: []Direction{Down, Left, Up, Right},
+		},
+		orderedmap.Pair[string, Gesture]{
+			Key:   "下右上左",
+			Value: []Direction{Down, Right, Up, Left},
+		},
+		orderedmap.Pair[string, Gesture]{
+			Key:   "左下上右",
+			Value: []Direction{Left, Down, Up, Right},
+		},
+		orderedmap.Pair[string, Gesture]{
+			Key:   "左上右下",
+			Value: []Direction{Left, Up, Right, Down},
+		},
+		// 三方向
+		orderedmap.Pair[string, Gesture]{
+			Key:   "上左下",
+			Value: []Direction{Up, Left, Down},
+		},
+		orderedmap.Pair[string, Gesture]{
+			Key:   "上右下",
+			Value: []Direction{Up, Right, Down},
+		},
+		orderedmap.Pair[string, Gesture]{
+			Key:   "下右上",
+			Value: []Direction{Down, Right, Up},
+		},
+		orderedmap.Pair[string, Gesture]{
+			Key:   "下左上",
+			Value: []Direction{Down, Left, Up},
+		},
+		orderedmap.Pair[string, Gesture]{
+			Key:   "左上右",
+			Value: []Direction{Left, Up, Right},
+		},
+		orderedmap.Pair[string, Gesture]{
+			Key:   "左下右",
+			Value: []Direction{Left, Down, Right},
+		},
+		orderedmap.Pair[string, Gesture]{
+			Key:   "右上左",
+			Value: []Direction{Right, Up, Left},
+		},
+		orderedmap.Pair[string, Gesture]{
+			Key:   "右下左",
+			Value: []Direction{Right, Down, Left},
+		},
+		// 双方向
+		orderedmap.Pair[string, Gesture]{
+			Key:   "上下",
+			Value: []Direction{Up, Down},
+		},
+		orderedmap.Pair[string, Gesture]{
+			Key:   "左右",
+			Value: []Direction{Left, Right},
+		},
+		orderedmap.Pair[string, Gesture]{
+			Key:   "上左",
+			Value: []Direction{Up, Left},
+		},
+		orderedmap.Pair[string, Gesture]{
+			Key:   "上右",
+			Value: []Direction{Up, Right},
+		},
+		orderedmap.Pair[string, Gesture]{
+			Key:   "下左",
+			Value: []Direction{Down, Left},
+		},
+		orderedmap.Pair[string, Gesture]{
+			Key:   "下右",
+			Value: []Direction{Down, Right},
+		},
+		// 单方向
+		orderedmap.Pair[string, Gesture]{
+			Key:   "上",
 			Value: []Direction{Up},
 		},
 		orderedmap.Pair[string, Gesture]{
-			Key:   "Down",
+			Key:   "下",
 			Value: []Direction{Down},
 		},
 		orderedmap.Pair[string, Gesture]{
-			Key:   "Left",
+			Key:   "左",
 			Value: []Direction{Left},
 		},
 		orderedmap.Pair[string, Gesture]{
-			Key:   "Right",
+			Key:   "右",
 			Value: []Direction{Right},
 		},
 	))
