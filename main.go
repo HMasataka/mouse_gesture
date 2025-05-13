f7ed0e8349e0c57fdb2af99821b2d5e61370bc72
HMasataka <masataka.hisasue@gmail.com>
2025-05-13 14:40:31 +0900

    Feat window

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import "github.com/hajimehoshi/ebiten/v2"
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	println(ebiten.CursorPosition())
+	ebiten.SetCursorMode(ebiten.CursorModeHidden)
+	ebiten.SetCursorMode(ebiten.CursorModeVisible)
+
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth, outsideHeight
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		panic(err)
+	}
 }
